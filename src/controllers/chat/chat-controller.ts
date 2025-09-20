ef8a0cd3eabed0d3fdb0bfdd50603525b878ed52
yashrajvrma <vermayashraj2005@gmail.com>
2025-09-21 00:03:20 +0530

    first checkpoint

--- src/controllers/chat/chat-controller.ts
+++ src/controllers/chat/chat-controller.ts
@@ -1,10 +1,8 @@
 import type { Request, Response } from "express";
 import ApiError from "../../utils/api-error.js";
 import prisma from "../../config/db.js";
-
 import ApiResponse from "../../utils/api-response.js";
 import AsyncHandler from "../../utils/async-handler.js";
-
 import { GoogleGenAI } from "@google/genai";
 
 export const createChat = AsyncHandler(async (req, res) => {
@@ -59,4 +57,49 @@ export const createChat = AsyncHandler(async (req, res) => {
       title: chat.title,
     })
   );
+});
+
+export const generateMsg = AsyncHandler(async (req, res) => {
+  const { chatId } = req.query;
+
+  const userId = req.user?.id!;
+
+  if (!chatId) {
+    throw new ApiError(400, "Chat id is required");
+  }
+
+  const isChatIdValid = await prisma.chat.findUnique({
+    where: {
+      id: chatId as string,
+      userId: userId,
+    },
+  });
+
+  if (!chatId) {
+    throw new ApiError(400, "Invalid chatId");
+  }
+
+  const message = await prisma.message.findMany({
+    where: {
+      chatId: chatId as string,
+    },
+    orderBy: {
+      createdAt: "asc",
+    },
+  });
+
+  console.log("message is", message);
+
+  if (!message || message.length === 0) {
+    throw new ApiError(400, "No chat message exist");
+  }
+
+  if (message.length === 1 && message[0]?.role === "USER") {
+  }
+  // return res.json(
+  //   new ApiResponse(200, {
+  //     message,
+  //   })
+
+  // );
 });
--- src/routes/chat-routes.ts
+++ src/routes/chat-routes.ts
@@ -1,10 +1,14 @@
 import { verifyJWT } from "./../middlewares/auth-middleware.js";
 import { Router } from "express";
 
-import { createChat } from "../controllers/chat/chat-controller.js";
+import {
+  createChat,
+  generateMsg,
+} from "../controllers/chat/chat-controller.js";
 
 const router = Router();
 
 router.route("/create-chat").post(verifyJWT, createChat);
+router.route("/").post(verifyJWT, generateMsg);
 
 export default router;
